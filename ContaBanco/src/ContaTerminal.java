3828b6e2ca5df460d783e89cf15803091f11e312
carlos moraes <carlosmoraes_sis@hotmail.com>
2024-07-13 13:55:45 -0400

    Implementação da Classe Conta Terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner terminal = new Scanner(System.in);
+
+        //Exibir as mensagens para o usuário
+        System.out.println("Por favor,digite o número da conta:");
+        int numero_conta = terminal.nextInt();
+
+        System.out.println("Por favor,digite o número da agência:");
+        String numero_agencia = terminal.next();
+
+        System.out.println("Por favor,digite o nome do Cliente:");
+        String nome_Cliente = terminal.next();
+
+        System.out.println("Por favor,digite o valor do Saldo da conta");
+        double saldo_conta = terminal.nextDouble();
+
+
+        System.out.println("Olá "  +nome_Cliente+ 
+        ", obrigado por criar uma conta em nosso banco,sua agência é " + numero_agencia+ ", conta " +numero_conta + " e seu saldo é R$ "+saldo_conta+ 
+         " já está disponível para saque");
     }
 }
